f7caf3242a20a2d7c1a95188c59887fa26f8cbe5
Felix Stavonhagen <felixstavonhagen@gmail.com>
2024-10-29 12:56:18 +0100

    add: adress check, fetching ens name and pfp

--- components/Send/RecipientInput.tsx
+++ components/Send/RecipientInput.tsx
@@ -3,11 +3,23 @@ import { View, TextInput, Alert, Image } from 'react-native';
 import FText from '~/components/Text/FText';
 import { Button } from '~/components/Button';
 
+import { http, createPublicClient } from 'viem';
+import { mainnet } from 'viem/chains';
+import { normalize } from 'viem/ens';
+import { isAddress } from 'viem';
+
+const client = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+});
+
 const RecipientInput = () => {
   const [recipientAddress, setRecipientAddress] = useState('');
+  const [ensAvatar, setEnsAvatar] = useState<string | null>(null);
+  const [ensName, setEnsName] = useState<string | null>(null);
 
   const handleSendFunds = () => {
-    if (recipientAddress) {
+    if (isAddress(recipientAddress)) {
       // Logic to send funds, this could involve interacting with a wallet or smart contract
       Alert.alert('Sending Funds', `Funds will be sent to ${recipientAddress}`);
     } else {
@@ -15,25 +27,45 @@ const RecipientInput = () => {
     }
   };
 
+  const isValidAddress = isAddress(recipientAddress);
+
+  const getEnsNameAndAvatar = async () => {
+    const ensName = await client.getEnsName({ address: recipientAddress as `0x${string}` });
+    if (ensName) {
+      console.log('ENS Name:', ensName);
+      setEnsName(ensName);
+      const ensAvatar = await client.getEnsAvatar({ name: normalize(ensName) });
+      setEnsAvatar(ensAvatar);
+      console.log('ENS Avatar:', ensAvatar);
+    } else {
+      console.log('ENS Name not found.');
+    }
+  };
+
   return (
     <View className="items-center rounded-lg bg-primary p-4">
       <View className="mb-4 flex-row items-center">
         <FText className="text-white">Recipient</FText>
-        <Image
-          source={{
-            uri: 'https://pbs.twimg.com/profile_images/1709489482414624768/ncqZhUxo_400x400.jpg',
-          }}
-          className="ml-4 h-16 w-16 rounded-full"
-        />
+        {ensName && <FText className="ml-auto text-white"> : {ensName}</FText>}
+        {ensAvatar && (
+          <Image
+            source={{
+              uri: ensAvatar,
+            }}
+            className="ml-4 h-16 w-16 rounded-full"
+          />
+        )}
       </View>
       <TextInput
-        className="mb-4 w-full rounded-md bg-blue-700 p-3 text-white"
+        className={`mb-4 w-full rounded-md bg-background p-3 text-white ${isValidAddress ? 'text-success' : 'text-error'}`}
         placeholder="0x1234...abcd"
         placeholderTextColor="#888"
         value={recipientAddress}
-        onChangeText={setRecipientAddress}
+        onChangeText={(text) => {
+          setRecipientAddress(text);
+        }}
       />
-      <Button title="Send Funds" onPress={handleSendFunds} />
+      <Button title="Get ENS name and avatar" onPress={getEnsNameAndAvatar} />
     </View>
   );
 };
