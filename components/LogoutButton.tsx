cc600c0cb30ba829e20b6415a18e94dae2181dbe
bendsp <benjy.desprets@gmail.com>
2024-11-20 20:29:22 -0500

    add: LogoutButton

---
+++ components/LogoutButton.tsx
@@ -0,0 +1,22 @@
+import { usePrivy } from '@privy-io/expo';
+import React from 'react';
+import { Alert } from 'react-native';
+
+import { Button } from './Button';
+
+export const LogoutButton = () => {
+    const { logout } = usePrivy();
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+            Alert.alert('Success', 'Logout successful!');
+        } catch (error: any) {
+            Alert.alert('Error', 'Failed to logout: ' + error.message);
+        }
+    };
+
+    return (
+        <Button onPress={handleLogout} className="bg-primary" title="Logout" />
+    )
+};
