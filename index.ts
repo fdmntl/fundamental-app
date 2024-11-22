ae5cfa60eb2180aa22ca0c519145150984aed460
bendsp <benjy.desprets@gmail.com>
2024-11-22 00:18:26 -0500

    update: override atob() to fix base64 decoding issues on Android

--- index.ts
+++ index.ts
@@ -4,5 +4,23 @@
 import 'fast-text-encoding';
 import 'react-native-get-random-values';
 import '@ethersproject/shims';
-// import '@thirdweb-dev/react-native-adapter';
+
+// Override 'atob' to fix base64 decoding issues on Android
+if (typeof atob === 'undefined') {
+  global.atob = function (input) {
+    return Buffer.from(input, 'base64').toString('binary');
+  };
+}
+
+// If 'atob' exists but has issues, override it
+const _atob = global.atob;
+
+global.atob = (val) => {
+  try {
+    return _atob(val);
+  } catch (e) {
+    return _atob(val + '=');
+  }
+};
+
 import 'expo-router/entry';
