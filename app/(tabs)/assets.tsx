6492906dbeee82ea668f0dd5183c800e7e01e4e4
Felix Stavonhagen <felixstavonhagen@gmail.com>
2024-12-17 21:19:18 +0100

    add: data as param to graph and mock data generation comment at bottom

--- app/(tabs)/assets.tsx
+++ app/(tabs)/assets.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Frame } from '~/components/Wrappers/Frame';
 import { HeaderBar } from '~/components/HeaderBar';
 import Graph from '~/components/Graph';
+import { DataPoint } from '~/types/data';
 
 export default function Assets() {
+  const [allData] = useState<DataPoint[]>([]);
+
   return (
     <Frame>
       <HeaderBar title="Assets" />
-      <Graph />
+      <Graph allData={allData} />
     </Frame>
   );
 }
+
+// // Mock data generation
+// const [allData] = useState<DataPoint[]>(
+//   Array.from({ length: 365 }, (_, i) => ({
+//     value: Math.floor(Math.random() * (2763 - 1000 + 1)) + 1000, // Random price
+//     label: new Date(Date.now() - i * 24 * 60 * 60 * 1000).toISOString(), // ISO date
+//   }))
+// );
