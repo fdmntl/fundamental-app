bd2854a6db1c38728cf335620496aa3fd5c00104
Paul <paul.thiebault@epitech.eu>
2025-01-13 01:12:34 +0000

    add: chevron icon to token picker

--- components/Send/AmountInput.tsx
+++ components/Send/AmountInput.tsx
@@ -65,6 +65,7 @@ export const AmountInput = ({
           <FText className="text-info" bold>
             {selectedToken?.symbol || 'Select Token'}
           </FText>
+          <Feather name="chevron-down" size={28} className="text-neutral" />
         </TouchableOpacity>
       </View>
       <View className="-mt-2 flex-row items-center">
