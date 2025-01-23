608232afc56e6d379b2b647f579e41a2277163e9
bendsp <benjy.desprets@gmail.com>
2025-01-23 17:18:00 -0500

    remove: leftover signCowQuote comments

--- services/CoW/signCowQuote.ts
+++ services/CoW/signCowQuote.ts
@@ -22,9 +22,4 @@ export const signCowQuote = async (
   };
   const signature = await OrderSigningUtils.signOrder(order, SupportedChainId.BASE, signer);
   return signature;
-  // const order: Order = quote as Order;
-  // const digest = hashOrder(domain(1, '0x9008D19f58AAbD9eD0D60971565AA8510560ab41'), order);
-  // const signature = await signMessage(provider, digest);
-  // console.log('Signature:', signature);
-  // return signature;
 };
