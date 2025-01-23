2f8cda7163e3d4e48ff2d229ce3f152a33a7892b
Paul <paul.thiebault@epitech.eu>
2025-01-23 14:38:47 +0000

    ad: Frame in Profile&Settings

--- app/(drawer)/settings.tsx
+++ app/(drawer)/settings.tsx
@@ -3,17 +3,20 @@ import { View } from 'react-native';
 
 import { FText } from '~/components/Text/FText';
 import { FTitle } from '~/components/Text/FTitle';
+import { Frame } from '~/components/Wrappers/Frame';
 
 const Settings = () => {
   return (
-    <View className="flex-1 items-center justify-center">
-      <FTitle className="text-4xl">These are the settings</FTitle>
-      <Link href="/" className="mt-2">
-        <FText medium italic>
-          Go to home screen!
-        </FText>
-      </Link>
-    </View>
+    <Frame>
+      <View className="flex-1 items-center justify-center">
+        <FTitle className="text-4xl">These are the settings</FTitle>
+        <Link href="/" className="mt-2">
+          <FText medium italic>
+            Go to home screen!
+          </FText>
+        </Link>
+      </View>
+    </Frame>
   );
 };
 
