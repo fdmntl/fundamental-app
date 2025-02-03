d6affc78a5eab70a5a079574f4e8f22fe77dbaa0
bendsp <benjy.desprets@gmail.com>
2025-02-03 00:03:34 -0500

    add: createCowAppdata

---
+++ services/CoW/createCowAppdata.ts
@@ -0,0 +1,20 @@
+import { MetadataApi, latest } from '@cowprotocol/app-data';
+
+export const createCowAppdata = async () => {
+  const metadataApi = new MetadataApi();
+  const appCode = 'Fundamental';
+  const environment = 'prod';
+  const referrer = { address: '0x4dcba6746997427dac9341c2a007f10d673ad878' };
+  const quoteAppDoc: latest.Quote = { slippageBips: 0.5 };
+  const orderClass: latest.OrderClass = { orderClass: 'market' };
+  const appDataDoc = await metadataApi.generateAppDataDoc({
+    appCode,
+    environment,
+    metadata: {
+      referrer,
+      quote: quoteAppDoc,
+      orderClass,
+    },
+  });
+  return appDataDoc;
+};
