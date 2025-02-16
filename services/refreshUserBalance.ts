ce5d2c04cfc35969bfffb9e5a28f047ceb38f777
Léonard Verschuere <leonard.verschuere@epitech.eu>
2025-02-16 22:40:30 +0000

    add: laid the bases for the refresh function

--- services/refreshUserBalance.ts
+++ services/refreshUserBalance.ts
@@ -1,6 +1,24 @@
-import { supabase } from '~/supabaseConfig';
-import { SubscriptionData } from '~/types/supabaseTypes';
+import { useState } from "react";
+import { supabase } from "~/supabaseConfig";
+import { useSupabaseUser } from "./Supabase/useSupabaseUser";
+import { useAppData } from "../components/Wrappers/AppData";
+import { Privy } from '~/types/privy';
+import { Token, User } from '~/types/supabaseTypes';
 
 // The database's Users table contains up-to-date and refreshing token balances. These balances should also be refreshable by the app, should a user have just made a trade or received a payment
+export const refreshUserBalances = async (user: User) => {
+  try {
+      console.log('Fetching user data...');
+      console.log('User:', user);
 
-// export const refreshUserBalances = async () => 
+      if (!user || !user.wallet_address) {
+          console.error("❌ No valid user address found.");
+          return;
+      }
+      console.log('Fetching user data for address:', user.wallet_address);
+      // Fetch balance logic here...
+
+  } catch (error) {
+      console.error("Error in refreshUserBalances:", error);
+  }
+};
