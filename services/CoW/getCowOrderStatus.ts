d508333653a3fd46b4ae18a7bc440e1a48e220ff
bendsp <benjy.desprets@gmail.com>
2025-02-18 12:28:58 -0500

    update: remove logging and add comments to getCowOrderStatus

--- services/CoW/getCowOrderStatus.ts
+++ services/CoW/getCowOrderStatus.ts
@@ -5,10 +5,10 @@ const orderBookApi = new OrderBookApi({ chainId: SupportedChainId.BASE });
 export const getCowOrderStatus = async (orderUid: string) => {
   try {
     console.log('Getting order status for:', orderUid);
+    // order conains the order details, trades contains the trades that have been executed as part of that order
+    // the information you want is likely in order.
     const order = await orderBookApi.getOrder(orderUid);
     const trades = await orderBookApi.getTrades({ orderUid });
-    console.log('Order:', order);
-    console.log('Trades:', trades);
     return { order, trades };
   } catch (error) {
     console.error('Error getting order status:', error);
