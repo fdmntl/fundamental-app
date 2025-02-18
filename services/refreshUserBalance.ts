9eb77729326ba76099efbce14dd434b179d0c4ce
Léonard Verschuere <leonard.verschuere@epitech.eu>
2025-02-18 11:37:14 +0000

    add: the db is now actualised before updating the balance

--- services/refreshUserBalance.ts
+++ services/refreshUserBalance.ts
@@ -1,45 +1,46 @@
-import { useState } from "react";
 import { supabase } from "~/supabaseConfig";
-import { useSupabaseUser } from "./Supabase/useSupabaseUser";
-import { useAppData } from "../components/Wrappers/AppData";
-import { Privy } from '~/types/privy';
-import { Token, User } from '~/types/supabaseTypes';
+import { User } from '~/types/supabaseTypes';
+import axios from 'axios';
 
 
 
 const fetchUserBalancesFromSupabase = async (walletAddress: string) => {
   // The database's Users table contains up-to-date and refreshing token balances. These balances should also be refreshable by the app, should a user have just made a trade or received a payment
+  // fetch the users balance from our server
+  await axios.get(`https://fundamental-api.netlify.app/.netlify/functions/updateTokenBalances/${walletAddress}`)
+    .catch((error) => {
+      console.error('Error fetching balance:', error.message);
+      throw error;
+    });
+    console.log("DB has been updated with new balances");
     const { data, error } = await supabase
       .from('users')
-      .select('balances') // Select only the balances column
-      .eq('wallet_address', walletAddress) // Filter by user ID
-      .single(); // Ensures only one result is returned
-
+      .select('balances')
+      .eq('wallet_address', walletAddress)
+      .single();
     if (error) {
-      console.error('Error fetching balance:', error.message);
-      return null;
+        console.error("Error fetching balances from Supabase:", error.message);
+        throw error;
     }
     return data?.balances || [];
-  };
+};
 
 export const refreshUserBalances = async (user: User, updateUser: (updates: Partial<User>) => void) => {
   try {
       console.log('Fetching user data for address:', user.wallet_address);
       if (!user || !user.wallet_address) {
-        console.error("❌ No valid user address found.");
+        console.error("No valid user address found.");
         return;
       }
-      // Simulate fetching new balance data from an API
       const newBalances = await fetchUserBalancesFromSupabase(user.wallet_address);
-      // Check if the balances array is empty (indicating no balances found)
       if (!newBalances || newBalances.length === 0) {
-        console.error("❌ No balances found for user:", user.wallet_address);
+        console.error("No balances found for user:", user.wallet_address);
         return;
       }
       console.log("✅ New balances fetched:", newBalances);
       // Update the user state with the new balance
       updateUser({ balances: newBalances });
   } catch (error) {
-      console.error("❌ Error in refreshUserBalances:", error);
+      console.error("Error in refreshUserBalances:", error);
   }
 };
