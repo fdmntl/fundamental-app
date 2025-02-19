d87954fb0a3ee6e80550443aabc2ca4ef2fd117a
bendsp <benjy.desprets@gmail.com>
2025-02-18 22:05:00 -0500

    update: remove visibility toggle and colored titlebar from Container, added no title support

--- components/Container.tsx
+++ components/Container.tsx
@@ -10,20 +10,22 @@ interface ContainerProps {
 }
 
 export const Container = ({ title, className, children }: ContainerProps) => {
-  const [isOpen, setIsOpen] = useState(true);
-  const toggleOpen = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-    <View className={`${className} rounded-xl bg-content`}>
-      <TouchableOpacity
-        onPress={toggleOpen}
-        className={`${isOpen ? 'rounded-t-xl' : 'rounded-xl'} bg-primary px-4 py-2`}>
-        <FText className="text-white" bold>
-          {title}
-        </FText>
-      </TouchableOpacity>
-      {isOpen && <View className="p-4">{children}</View>}
-    </View>
-  );
+  if (title) {
+    return (
+      <View className={`${className} rounded-xl bg-content`}>
+        <View className={`rounded-t-xl px-4 py-2`}>
+          <FText className="text-xl text-primary" bold>
+            {title}
+          </FText>
+        </View>
+        <View className="p-4">{children}</View>
+      </View>
+    );
+  } else {
+    return (
+      <View className={`${className} rounded-xl bg-content`}>
+        <View className="p-4">{children}</View>
+      </View>
+    );
+  }
 };
