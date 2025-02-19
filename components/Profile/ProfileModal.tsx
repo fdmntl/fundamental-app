1c9396ec525a2d59182bf61e2078a7d022a4d35b
bendsp <benjy.desprets@gmail.com>
2025-02-18 22:32:33 -0500

    update: added truncated address and moved QR code to expanded profile modal

--- components/Profile/ProfileModal.tsx
+++ components/Profile/ProfileModal.tsx
@@ -7,13 +7,15 @@ import { useAppData } from '../Wrappers/AppData';
 
 import { Button } from '../Button';
 import { FText } from '../Text/FText';
+import QRCode from 'react-native-qrcode-svg';
 
 export const ProfileModal = () => {
   const [isExpanded, setisExpanded] = useState(false);
   const toggleModal = () => setisExpanded(!isExpanded);
   const router = useRouter();
   const { privy, user } = useAppData();
-
+  const truncatedAddress =
+    privy.wallet?.account?.address.slice(0, 6) + '...' + privy.wallet?.account?.address.slice(-4);
   return (
     <View className="w-full">
       <TouchableOpacity onPress={toggleModal} className="w-full rounded p-2">
@@ -24,13 +26,20 @@ export const ProfileModal = () => {
       </TouchableOpacity>
       <Modal visible={isExpanded} animationType="fade" transparent onRequestClose={toggleModal}>
         <TouchableWithoutFeedback onPress={toggleModal}>
-          <View className="flex-1 items-center justify-center bg-[rgba(0,0,0,0.7)]">
-            <View className="w-4/5 items-center rounded-lg bg-[rgba(0,0,0,0.9)] p-5">
-              <FText className="text-lg font-bold text-white">FULL VIEW</FText>
-              <TouchableOpacity onPress={toggleModal} className="mt-4 rounded bg-red-500 p-2">
-                <FText className="text-white">Close</FText>
+          <View className="flex-1 items-center justify-center bg-[rgba(0,0,0,0.5)]">
+            <Container title="Your Profile" className="w-11/12 rounded-lg bg-content p-4">
+              <TouchableOpacity>
+                <FText className="text-center text-text">
+                  @{user.ens} | {user.ens}.fdmntl.eth
+                </FText>
               </TouchableOpacity>
-            </View>
+              <View className="mt-8 flex items-center justify-center">
+                <QRCode value={user.wallet_address} size={200} />
+              </View>
+              <TouchableOpacity>
+                <FText className="mt-2 text-center text-text">{truncatedAddress}</FText>
+              </TouchableOpacity>
+            </Container>
           </View>
         </TouchableWithoutFeedback>
       </Modal>
