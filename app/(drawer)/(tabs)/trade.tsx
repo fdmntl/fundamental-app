2a364f8d0656894af93724491583cfae6bbe099d
Léonard Verschuere <leonard.verschuere@epitech.eu>
2025-02-23 12:21:33 +0000

    add: the scroll down function on the trade page to refresh and update the balance

--- app/(drawer)/(tabs)/trade.tsx
+++ app/(drawer)/(tabs)/trade.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React from 'react';
+import { useCallback, useState } from 'react';
 import { View } from 'react-native';
 
 import { Button } from '~/components/Button';
@@ -7,10 +8,12 @@ import { AmountInput } from '~/components/Send/AmountInput';
 import { QuoteDisplay } from '~/components/Trade/QuoteDisplay';
 import { useAppData } from '~/components/Wrappers/AppData';
 import { Frame } from '~/components/Wrappers/Frame';
+import { refreshUserBalances } from '~/services/refreshUserBalance';
 import { Token } from '~/types/supabaseTypes';
+import { ScrollView, RefreshControl } from 'react-native-gesture-handler';
 
 export default function Trade() {
-  const { user, tokens } = useAppData();
+  const { user, tokens, updateUser } = useAppData();
 
   const [amount, setAmount] = useState(''); // Payment input value
   const [selectedToken, setSelectedToken] = useState<Token | null>(null); // Selected payment token
@@ -30,30 +33,49 @@ export default function Trade() {
     console.log('Selected Token:', selectedTokenBalance);
   };
 
+  const [refreshing, setRefreshing] = React.useState(false);
+  const onRefresh = useCallback(async () => {
+    // Ensure refresh only happens when triggered otherwise it refreshes the user balances on every render (not cool)
+    if (!refreshing) {
+      setRefreshing(true);
+      try {
+        await refreshUserBalances(user, updateUser);
+      } finally {
+        setRefreshing(false);
+      }
+    }
+  }, [refreshing, refreshUserBalances]);
+
   return (
     <Frame>
       <HeaderBar title="Trade" />
-      <View className="flex-1 gap-4">
-        {/* Amount Input for "You Pay" */}
-        <AmountInput
-          value={amount}
-          onChange={(value) => setAmount(value)}
-          tokens={possessedTokens}
-          user={user}
-          selectedTokenBalance={selectedTokenBalance}
-          onTokenChange={(token) => setSelectedToken(token)}
-          title="You Pay"
-        />
+      <ScrollView showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>
+        }
+      >
+        <View className="flex-1 gap-4">
+          {/* Amount Input for "You Pay" */}
+          <AmountInput
+            value={amount}
+            onChange={(value) => setAmount(value)}
+            tokens={possessedTokens}
+            user={user}
+            selectedTokenBalance={selectedTokenBalance}
+            onTokenChange={(token) => setSelectedToken(token)}
+            title="You Pay"
+          />
 
-        {/* Quote Display */}
-        <QuoteDisplay
-          tokens={possessedTokens}
-          user={user}
-          selectedTokenBalance={selectedTokenBalance}
-          youPayValue={parseFloat(amount) || 0}
-          youPayToken={selectedToken || possessedTokens[0]}
-        />
-      </View>
+          {/* Quote Display */}
+          <QuoteDisplay
+            tokens={possessedTokens}
+            user={user}
+            selectedTokenBalance={selectedTokenBalance}
+            youPayValue={parseFloat(amount) || 0}
+            youPayToken={selectedToken || possessedTokens[0]}
+          />
+        </View>
+      </ScrollView>
       <View className="absolute bottom-[6rem] w-full items-center">
         <Button
           title="Trade"
