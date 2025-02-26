a8b119999693d53eb6f38d0b67531a5e9252a4ea
Paul <paul.thiebault@epitech.eu>
2025-02-26 19:26:06 +0000

    add: order polling edge cases & toast implementation

--- components/Trade/OrderStatusPolling.tsx
+++ components/Trade/OrderStatusPolling.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useRef } from 'react';
+import Toast from 'react-native-toast-message';
 
+import { useAppData } from '~/components/Wrappers/AppData';
 import { getCowOrderStatus } from '~/services/CoW/getCowOrderStatus';
+import { digitsToAmount } from '~/utils/helpers/tokens/digitsToAmount';
 
 interface OrderStatusPollingProps {
   orderUid: string;
@@ -8,32 +11,61 @@ interface OrderStatusPollingProps {
 
 export const OrderStatusPolling = ({ orderUid }: OrderStatusPollingProps) => {
   const isPolling = useRef(true);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const { getToken } = useAppData();
+
+  const scheduleNextPoll = () => {
+    if (isPolling.current) {
+      timeoutRef.current = setTimeout(pollOrderStatus, 2000);
+    }
+  };
+
+  const pollOrderStatus = async () => {
+    try {
+      const response = await getCowOrderStatus(orderUid);
+      if (!response) {
+        console.log('Order status is null');
+        return scheduleNextPoll();
+      }
+
+      const { order } = response;
+      console.log('order: ', order);
+
+      // If expired, stop polling
+      if (order.status === 'expired') {
+        console.log('Order expired, stopping polling.');
+        return;
+      }
+
+      // If not fulfilled, schedule next poll (fail-safe)
+      if (order.status !== 'fulfilled') {
+        return scheduleNextPoll();
+      }
+
+      // If fulfilled, show toast
+      const token = getToken(order.sellToken);
+      const amount = Number(order.buyAmount);
+      const price = amount && token && digitsToAmount(amount, token);
+
+      Toast.show({
+        type: 'fundamental',
+        text1: 'Order Fulfilled!',
+        text2: token && price ? `You bought ${price} ${token.symbol}` : undefined,
+      });
+    } catch (error) {
+      console.error('Error polling order status:', error);
+    }
+    scheduleNextPoll();
+  };
 
   useEffect(() => {
-    const pollOrderStatus = async () => {
-      try {
-        const response = await getCowOrderStatus(orderUid);
-        if (response) {
-          const { order } = response;
-          console.log('order: ', order);
-          // TODO: handle order status, if fulfilled, use toast, else poll again
-        } else {
-          console.log('Order status is null');
-        }
-      } catch (error) {
-        console.error('Error polling order status:', error);
-      }
-
-      if (isPolling.current) {
-        // Poll every 2 seconds
-        setTimeout(pollOrderStatus, 2000);
-      }
-    };
-
     pollOrderStatus();
 
     return () => {
       isPolling.current = false;
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
     };
   }, [orderUid]);
 
