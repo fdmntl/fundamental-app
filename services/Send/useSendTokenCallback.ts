6dbeb8ad7ac53f9e23aa74deba2d55a9557ee630
Paul <paul.thiebault@epitech.eu>
2025-03-01 16:41:09 +0100

    add: toast in send method + async

--- services/Send/useSendTokenCallback.ts
+++ services/Send/useSendTokenCallback.ts
@@ -1,5 +1,6 @@
 import { EmbeddedWalletState } from '@privy-io/expo';
 import { useCallback } from 'react';
+import Toast from 'react-native-toast-message';
 
 import { sendERC20, sendETH } from '~/services/viemService';
 import { Token } from '~/types/supabaseTypes';
@@ -20,26 +21,37 @@ export const useSendTokenCallback = ({
   amount,
   isInputValid,
 }: useSendTokenProps) => {
-  const handleSendTokenCallback = useCallback(() => {
+  const handleSendTokenCallback = useCallback(async () => {
     if (!isInputValid || !selectedToken || !wallet || wallet.status !== 'connected') {
       return;
     }
-    if (selectedToken.address === '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee') {
-      // ETH is not an ERC-20 token, so we need to handle it separately. We represent it with 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-      sendETH(
-        wallet.provider,
-        recipient,
-        BigInt(amountToDigits(parseFloat(amount), selectedToken))
-        // Update the user's balance in the database
-      );
-    } else {
-      sendERC20(
-        wallet.provider,
-        selectedToken.address as `0x${string}`,
-        recipient as `0x${string}`,
-        BigInt(amountToDigits(parseFloat(amount), selectedToken))
-        // Update the user's balance in the database
-      );
+
+    const value = BigInt(amountToDigits(parseFloat(amount), selectedToken));
+
+    try {
+      if (selectedToken.address === '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee') {
+        // ETH is not an ERC-20 token, so we need to handle it separately. We represent it with 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        await sendETH(wallet.provider, recipient, value);
+      } else {
+        await sendERC20(
+          wallet.provider,
+          selectedToken.address as `0x${string}`,
+          recipient as `0x${string}`,
+          value
+        );
+      }
+      Toast.show({
+        type: 'success',
+        text1: 'Transaction Sent!',
+        text2: `You sent ${amount} ${selectedToken.symbol} to ${recipient}`,
+      });
+    } catch (error) {
+      console.error('Error sending token', error);
+      Toast.show({
+        type: 'error',
+        text1: 'Error Sending Transaction',
+        text2: 'Please try again later',
+      });
     }
   }, [amount, isInputValid, recipient, selectedToken, wallet]);
 
