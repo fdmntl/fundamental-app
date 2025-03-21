187ef994b3dfbf0ae2c9e3fee97b1e951df5c606
Paul <paul.thiebault@epitech.eu>
2025-03-21 01:25:00 +0000

    add: expo-linear-gradient & gradient button & gradient container border

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,4 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { View } from 'react-native';
 
 import { FText } from './Text/FText';
@@ -12,10 +13,17 @@ export const Container = ({ title, className, children }: ContainerProps) => {
   return (
     <View className={`${className} rounded-xl bg-content`}>
       {title && (
-        <View className="rounded-t-xl border-b-[3px] border-primary px-4 py-2">
-          <FText className="text-xl text-primary" bold>
+        <View>
+          <FText className="px-4 py-2 text-xl" bold>
             {title}
           </FText>
+
+          <LinearGradient
+            colors={['#6115B5', '#741AD9', '#8720FE', '#8435E0', '#A250F5', '#D455FF']}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            style={{ height: 3, width: '100%', borderRadius: 1 }}
+          />
         </View>
       )}
       <View className="p-4">{children}</View>
