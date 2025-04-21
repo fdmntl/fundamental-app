e7c1ddf21543634dd89f523738271235f8a0db91
bendsp <benjy.desprets@gmail.com>
2025-04-21 12:24:32 -0400

    update: minor fix

--- components/Wrappers/AuthProvider.tsx
+++ components/Wrappers/AuthProvider.tsx
@@ -17,7 +17,6 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const { user } = usePrivy();
   const wallet = useEmbeddedWallet();
-  //   const wallet = useEmbeddedWallet();
   const { login } = useLogin();
 
   const [authState, setAuthState] = useState<AuthState>({
