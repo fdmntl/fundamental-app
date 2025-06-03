ffbc23f8c5d12800a0def23fde3aafafc3f365a2
bendsp <benjy.desprets@gmail.com>
2025-06-03 15:11:52 -0400

    remove: unused imports

--- components/Transaction/TradeHistoryListView.tsx
+++ components/Transaction/TradeHistoryListView.tsx
@@ -1,5 +1,4 @@
-import { View, Animated } from 'react-native';
-import React from 'react';
+import { View } from 'react-native';
 
 import { FText } from '../Text/FText';
 import { TradeOrderItemSkeleton } from './TradeOrderItemSkeleton';
