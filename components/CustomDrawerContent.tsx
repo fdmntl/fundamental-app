8999a2e7d4183e1c6519051f9b1151d29ffd9328
Paul <paul.thiebault@epitech.eu>
2025-06-11 16:27:37 +0200

    add: manual icon for onboarding

--- components/CustomDrawerContent.tsx
+++ components/CustomDrawerContent.tsx
@@ -1,14 +1,23 @@
+import { Feather } from '@expo/vector-icons';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
-import { View, Image } from 'react-native';
+import { useState } from 'react';
+import { View, Image, TouchableOpacity } from 'react-native';
 
 import { LogoutButton } from './LogoutButton';
+import { OnboardingScreen } from './OnboardingSceen/OnboardingScreen';
 import { ProfileModal } from './Profile/ProfileModal';
 import ThemeToggle from './ThemeToggle';
 
 export default function CustomDrawerContent(props: any) {
+  const [showOnboarding, setShowOnboarding] = useState(false);
+  const toggleOnbaording = () => {
+    setShowOnboarding(!showOnboarding);
+  };
   return (
     <View className="flex-1 bg-background">
       <DrawerContentScrollView {...props} scrollEnabled={false} className="flex-1">
+        <OnboardingScreen visible={showOnboarding} onClose={() => setShowOnboarding(false)} />
+
         <View className="p-3">
           <Image
             source={require('../assets/fundamental-text.png')}
@@ -24,8 +33,11 @@ export default function CustomDrawerContent(props: any) {
           <ThemeToggle />
         </View>
       </DrawerContentScrollView>
-      <View className="p-8 pb-12">
+      <View className="flex flex-row justify-between p-8 pb-12">
         <LogoutButton />
+        <TouchableOpacity onPress={toggleOnbaording}>
+          <Feather name="book-open" size={42} className="text-text" />
+        </TouchableOpacity>
       </View>
     </View>
   );
