06d2af9ceefcf601fcd98bcf4f90be3ef65512bc
Léonard Verschuere <leonard.verschuere@epitech.eu>
2025-06-25 13:51:06 +0200

    add: added a FontScaling parameter so that we can manually disable the user's Fontscaling when needed (should avoid the user with very big scale to break the UI)

--- components/Text/FTitle.tsx
+++ components/Text/FTitle.tsx
@@ -3,11 +3,16 @@ import { Text } from 'react-native';
 interface TitleProps {
   children: React.ReactNode;
   className?: string;
+  allowFontScaling?: boolean;
 }
 
-export const FTitle = ({ children, className, ...props }: TitleProps) => {
+export const FTitle = ({ children, className, allowFontScaling = true, ...props }: TitleProps) => {
   return (
-    <Text className={`${className} text-text`} style={{ fontFamily: 'Inter_700Bold' }} {...props}>
+    <Text
+      className={`${className} text-text`}
+      style={{ fontFamily: 'Inter_700Bold' }}
+      allowFontScaling={allowFontScaling}
+      {...props}>
       {children}
     </Text>
   );
