20839ba8db112ac24a1c65d54ecbc1f71ba125ba
bendrsio <benjy.desprets@gmail.com>
2025-06-29 18:07:54 -0400

    useGradient and outline optionnal Container props

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,4 +1,5 @@
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import { FText } from './Text/FText';
 
@@ -7,17 +8,39 @@ interface ContainerProps {
   className?: string;
   children: React.ReactNode;
   noPadding?: boolean;
+  outline?: boolean;
+  useGradient?: boolean;
 }
 
-export const Container = ({ title, className, children, noPadding = false }: ContainerProps) => {
+export const Container = ({
+  title,
+  className,
+  children,
+  noPadding = false,
+  outline = false,
+  useGradient = false,
+}: ContainerProps) => {
+  const gradientColors = ['#741AD9', '#8720FE', '#A250F5'];
+
   return (
     <View className={`${className}`}>
       {title && (
-        <FText className="mb-2 text-2xl text-text" bold>
+        <FText className={`mb-2 text-2xl ${useGradient ? 'text-white' : 'text-text'}`} bold>
           {title}
         </FText>
       )}
-      <View className={`rounded-xl bg-content ${noPadding ? '' : 'p-0'}`}>
+      <View
+        className={`rounded-xl ${useGradient ? '' : 'bg-content'} overflow-hidden ${
+          outline ? 'border-2 border-primary' : ''
+        }`}>
+        {useGradient && (
+          <LinearGradient
+            colors={gradientColors}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            style={StyleSheet.absoluteFillObject}
+          />
+        )}
         <View className={noPadding ? '' : 'p-4'}>{children}</View>
       </View>
     </View>
